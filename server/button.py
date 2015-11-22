69e0c5e7d7247afa7f8560576a1d06d4f7956f8a
Radu <rvacaru@gmail.com>
2015-11-22 10:04:36 +0100

    improved cleanup

--- server/button.py
+++ server/button.py
@@ -11,3 +11,6 @@ def button_logic():
 
 def init(app):
 	backthread.start(app, button_logic)
+
+def cleanup(app):
+	backthread.stop(app)
--- server/motor.py
+++ server/motor.py
@@ -3,18 +3,14 @@
 from pi2go import pi2go
 import carLeds
 
-BNone = 0
-BLeft = 1
-BRight = 2
-
 currentSpeed = 30
 currentCommand = None
-currentBlinker = BNone
 
-def execute(cmd, speed = None):
+def execute(cmd, arg = None):
 	global currentCommand
 	# if cmd is "speed" just execute adjustSpeed
-	adjustSpeed(speed)
+	if cmd != "move":
+		adjustSpeed(arg) # for 'move' the arg is the angle not the speed
 	if cmd == "speed":
 		if currentCommand != None:
 			execute(currentCommand)
@@ -30,44 +26,87 @@ def execute(cmd, speed = None):
 		left()
 	if cmd == "right":
 		right()
+	if cmd == "move":
+		# if no arg specified for move then use by default 90 (straight forward)
+		if arg == None:
+			arg = 90
+		move(arg)
 	currentCommand = cmd
 
 def forward():
-	global currentSpeed, currentCommand, currentBlinker
+	global currentSpeed, currentCommand
 	pi2go.forward(currentSpeed)
-	resetBlinkers()
-	print "Forward ", currentSpeed
+	#print "Forward ", currentSpeed
 	
 def reverse():
 	global currentSpeed, currentCommand
 	pi2go.reverse(currentSpeed)
-	resetBlinkers()
-	print "Reverse ", currentSpeed
+	#print "Reverse ", currentSpeed
 	
 def left():
-	global currentSpeed, currentCommand, currentBlinker
-	#pi2go.spinLeft(currentSpeed)
-	pi2go.go(currentSpeed * 60/100, currentSpeed)
-	resetBlinkers()
-	carLeds.execute("left")
-	currentBlinker = BLeft
-	print "Left ", currentSpeed
+	global currentSpeed, currentCommand
+	pi2go.spinLeft(currentSpeed)
+	#print "Left ", currentSpeed
 	
 def right():
-	global currentSpeed, currentCommand, currentBlinker
-	#pi2go.spinRight(currentSpeed)
-	pi2go.go(currentSpeed, currentSpeed * 60/100)
-	resetBlinkers()
-	carLeds.execute("right")
-	currentBlinker = BRight
-	print "Right ", currentSpeed
+	global currentSpeed, currentCommand
+	pi2go.spinRight(currentSpeed)
+	#print "Right ", currentSpeed
 	
 def stop():
 	global currentCommand
 	pi2go.stop()
-	resetBlinkers()
 	carLeds.execute("brake")
-	print "Stopped"
+	#print "Stopped"
+	
+#	 90
+#  	  |
+#180 --- 0
+#     |
+#    270
+# give 20deg space for going straight in each direction
+# between 0 and 180 we move forward (+)
+# between 181 and 360 we move backward (-)
+# between 80 and 110 we go straight forward
+def move(angle):
+	global currentSpeed, currentCommand
+	if angle >= 10 or angle >= 350:
+		right()
+		#print "full right"
+	
+	if angle > 10 and angle < 80:
+		pi2go.turnForward(1.2*currentSpeed, currentSpeed * angle2SpeedPercent(angle))
+		#print "forward right"
+
+	if angle >= 80 and angle <= 100:
+		forward()
+		#print "full forward"
+
+	if angle > 100 and angle < 170:
+		pi2go.turnForward(currentSpeed * angle2SpeedPercent(angle), 1.2*currentSpeed)
+		#print "forward left"
+
+	if angle >= 170 and angle <= 190:
+		left()
+		#print "full left"
+
+	if angle > 190 and angle < 260:
+		pi2go.turnReverse(currentSpeed * angle2SpeedPercent(angle), 1.2*currentSpeed)
+		#print "backward left"
+
+	if angle >= 260 and angle <= 280:
+		reverse()
+		#print "full backward"
+
+	if angle > 280 and angle < 350:
+		pi2go.turnReverse(1.2*currentSpeed, currentSpeed * angle2SpeedPercent(angle))
+		#print "backward right"
+	
+# convert a deg angle to percent of currentSpeed depending on the current quadrant
+def angle2SpeedPercent(angle):
+	p = 1.0 *(angle % 90) / 70
+	print p
+	return p
 	
 def adjustSpeed(speed):
 	global currentSpeed, currentCommand
@@ -80,11 +119,3 @@ def adjustSpeed(speed):
 	if speed == 0:
 		currentSpeed = 0
 	print "Current speed:", currentSpeed
-	
-def resetBlinkers():
-	global currentBlinker
-	if currentBlinker == BLeft:
-		carLeds.execute("left")
-	if currentBlinker == BRight:
-		carLeds.execute("right")
-	currentBlinker = BNone
