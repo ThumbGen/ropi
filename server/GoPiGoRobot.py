804c25eadb3320adaa886ebed277901bfc9f8814
Radu <rvacaru@gmail.com>
2016-03-05 18:39:55 +0100

    added support for various robots

---
+++ server/GoPiGoRobot.py
@@ -0,0 +1,58 @@
+from gopigo import gopigo
+
+def init():
+	pass
+	
+def cleanup():
+	pass
+	
+def setLED(led, red, green, blue):
+	pass
+
+def setAllLEDs(red, green, blue):
+	pass
+	
+def irLeft():
+	return False
+	
+def irRight():
+	return False
+	
+def irCentre():
+	return False
+	
+def irLeftLine():
+	return False
+	
+def irRightLine():
+	return False
+	
+def getSwitch():
+	return False
+	
+def getLight(index):
+	return 0
+	
+def forward(speed):
+	pass
+	
+def reverse(speed):
+	pass
+	
+def spinLeft(speed):
+	pass
+
+def spinRight(speed):
+	pass
+	
+def stop():
+	pass
+	
+def turnForward(leftSpeed, rightSpeed): 
+	pass
+
+def turnreverse(leftSpeed, rightSpeed): 
+	pass
+	
+def getDistance():
+	return 0
---
+++ server/configLoader.py
@@ -0,0 +1,39 @@
+import imp, os, json
+
+# robot is the global module which encapsulates the specific robot implementation (Pi2Go, GoPiGo, etc)
+robot = None
+# this is the json file where the configuration is loaded from
+configFile = "robotConfig.json";
+
+title = ""
+robotType = None
+
+def load():
+	global robot, title, robotType
+	
+	#read the robotconfig.json file and see what type of robot is configured
+	try:
+		with open(configFile) as data_file:
+			data = json.load(data_file)
+		
+		title = data["title"]
+		robotType = data["robotType"]		
+		robotFile = data["robotFile"]
+						
+		robot = imp.load_source("robot", robotFile)
+		# from this moment the robot import is available globally
+		print "==========================================================================="
+		print "Loaded " + title + " for " + robotType + " from " + robotFile
+		print "==========================================================================="
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
+	
+def save():
+	return
+	
+def getTitle():
+	return title
+
+def getRobotType():
+	return robotType
+
